c4adf3d7e21536c1f304e9ef57b6819a76e30176
Stephanie <stephnjonjo@gmail.com>
2024-10-13 15:44:14 +0300

    adds new codes

--- src/index.js
+++ src/index.js
@@ -1,45 +1,49 @@
 // index.js
 
-const ramenMenu = document.getElementById('ramen-menu')
-fetch('http://localhost:3000/ramens')
-  .then(res => res.json())
-  .then(renderRamens)
-
-function renderRamens(ramens){
-  ramens.forEach(renderRamen)
-}
-
-function renderRamen(ramen){
-  const ramenMenuDiv = document.getElementById("ramen-menu")
-  const ramenImage = document.createElement("img")
-  ramenImage.src=ramen.image
-  ramenMenuDiv.append(ramenImage)
-
-  ramenImage.addEventListener("click", () => ramenHandleClick(ramen))
-}
-
-
-
+const ramenMenu = document.getElementById('ramen-menu');
+const detailImage = document.querySelector("#ramen-detail .detail-image");
+const detailName = document.querySelector("#ramen-detail .name");
+const detailRestaurant = document.querySelector("#ramen-detail .restaurant");
+const detailRating = document.getElementById("rating-display");
+const detailComment = document.getElementById("comment-display");
+const newRamenForm = document.getElementById('new-ramen');
 
 
 // Callbacks
-const handleClick = (ramen) => {
+const handleClick = (ramen) => {  
   detailImage.src = ramen.image;
   detailName.textContent = ramen.name;
   detailRestaurant.textContent = ramen.restaurant;
-  detailComment.textContent = ramen.comment;
   detailRating.textContent = ramen.rating;
+  detailComment.textContent = ramen.comment;
   // Add code
 };
 
-
-
 const addSubmitListener = () => {
+  newRamenForm.addEventListener('submit', (event) => {
+    event.preventDefault();}
   // Add code
-}
+  )}
 
 const displayRamens = () => {
-  
+  fetch('http://localhost:3000/ramens')
+  .then(res => res.json())
+  .then(renderRamens)
+
+const renderRamens(ramens) => {
+  ramens.forEach(renderRamen)
+}}
+
+const renderRamen = (ramen) => {
+  const ramenImage = document.createElement("img");
+  ramenImage.src = ramen.image;
+  ramenImage.alt = ramen.name;
+  ramenMenu.append(ramenImage);
+
+
+ramenImage.addEventListener("click", () => handleClick(ramen))
+
+
   // Add code
 };
 
