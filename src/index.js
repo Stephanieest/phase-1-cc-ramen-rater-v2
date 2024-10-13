c43221ca4eaace0be3d92ea54d8dc93583a6f243
Stephanie <stephnjonjo@gmail.com>
2024-10-13 12:48:51 +0300

    adds new codes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
 // index.js
 
+const ramenMenu = document.getElementById('ramen-menu')
+fetch('http://localhost:3000/ramens')
+  .then(res => res.json())
+  .then(renderRamens)
+
+function renderRamens(ramens){
+  ramens.forEach(renderRamen)
+}
+
+function renderRamen(ramen){
+  const ramenMenuDiv = document.getElementById("ramen-menu")
+  const ramenImage = document.createElement("img")
+  ramenImage.src=ramen.image
+  ramenMenuDiv.append(ramenImage)
+
+  ramenImage.addEventListener("click", () => ramenHandleClick(ramen))
+}
+
+
+
+
+
 // Callbacks
 const handleClick = (ramen) => {
+  detailImage.src = ramen.image;
+  detailName.textContent = ramen.name;
+  detailRestaurant.textContent = ramen.restaurant;
+  detailComment.textContent = ramen.comment;
+  detailRating.textContent = ramen.rating;
   // Add code
 };
+
+
 
 const addSubmitListener = () => {
   // Add code
 }
 
 const displayRamens = () => {
+  
   // Add code
 };
 
